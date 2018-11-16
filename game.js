18ff07ccdeab06d6bd9a931e924b96b477ce690a
ChristinaLavingia <christinalavingia@gmail.com>
2018-11-15 18:36:30 -0800

    Updated code to reflect working game but no reset of player guesses.

--- game.js
+++ game.js
@@ -1,50 +1,53 @@
 //Generates the list of options for the computer's guesses
-
 var computerOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-//Sets our variables
-
+//Sets our variables for keeping track fo score
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-var playerGuess = [];
-var computerGuess = [];
+var playerGuessArray = [];
+var playerGuess;
 
-//This code will generate a random letter and store it for the computer guess
+//Generates a random letter and stores it for the computer guess
 window.onload = function() {
     var computerGuess = computerOptions [Math.floor(Math.random() * computerOptions.length)];
-}
+    console.log(computerGuess);
 
-document.onkeyup = function(event) {
+//Logs playerGuess
+ document.onkeyup = function(event) {
     var playerGuess = event.key;
-    playerGuess.push(playerGuess);
+    playerGuessArray.push(playerGuess);
+    var displayPlayerGuess = document.getElementById("lettersGuessed");
+    displayPlayerGuess.innerHTML += playerGuess;
+    var displayGuessesLeft = document.getElementById("guessesLeft");
+    displayGuessesLeft.innerHTML = guessesLeft;
 
-
-//Handles correct guesses
+// Handles correct guesses
     if ((playerGuess === computerGuess) && (guessesLeft > 0)) {
         wins++;
         guessesLeft = 9;
-        lettersGuessed.length = 0;
-        var computerGuess = computerOptions [Math.floor(Math.random() * computerOptions.length)];
+        playerGuessArray.length = 0;
+        var displayWins = document.getElementById("wins");
+        displayWins.innerHTML = wins;
+        computerGuess = computerOptions [Math.floor(Math.random() * computerOptions.length)];
+        console.log(wins);
 
-    } else if (playerGuess !== computerGuess) && (guessesLeft > 0) {
+    } else if ((playerGuess !== computerGuess) && (guessesLeft > 0)) {
         guessesLeft--;
 
     } else {
         losses++;
         lettersGuessed.length = 0;
         guessesLeft = 9;
-        var computerGuess = computerOptions [Math.floor(Math.random() * computerOptions.length)];
+        playerGuessArray.length = 0;
+        var displayLosses = document.getElementById("losses");
+        displayLosses.innerHTML = losses;
+        computerGuess = computerOptions [Math.floor(Math.random() * computerOptions.length)];
+        console.log(losses);
+        
+        }
+
     }
 
-    var html = "<p>Guess what letter I'm thinking of ...</p>" +
-                "<p>Wins: " + wins + "</p>" +
-                "<p>Losses: " + losses + "</p>" +
-                "<p>Guesses Left: " + guessesLeft + "</p>" +
-                "<p> Your Guesses So Far: " + playerGuess + "</p>";
-
-    document.querySelector("#game").innerHTML = html;
 }
-
-
-
+ 
