d4c0a0ead53c6908f84d1e3d738e3a6891279ee7
ChristinaLavingia <christinalavingia@gmail.com>
2018-11-17 14:39:10 -0800

    Updated JavaScript with working reset of player guesses.

--- assets/game.js
+++ assets/game.js
@@ -5,18 +5,18 @@ var computerOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-var playerGuessArray = [];
+var guessedLetters = [];
 var playerGuess;
 
 //Generates a random letter and stores it for the computer guess
 window.onload = function() {
     var computerGuess = computerOptions [Math.floor(Math.random() * computerOptions.length)];
-    console.log(computerGuess);
+    console.log(computerGuess);     
 
 //Logs playerGuess
  document.onkeyup = function(event) {
     var playerGuess = event.key;
-    playerGuessArray.push(playerGuess);
+    guessedLetters.push(playerGuess);
     var displayPlayerGuess = document.getElementById("lettersGuessed");
     displayPlayerGuess.innerHTML += playerGuess;
     var displayGuessesLeft = document.getElementById("guessesLeft");
@@ -26,7 +26,9 @@ window.onload = function() {
     if ((playerGuess === computerGuess) && (guessesLeft > 0)) {
         wins++;
         guessesLeft = 9;
-        playerGuessArray.length = 0;
+        guessedLetters = [];
+        var displayGuessedLetters = document.getElementById("lettersGuessed");
+        displayGuessedLetters.innerHTML = guessedLetters;
         var displayWins = document.getElementById("wins");
         displayWins.innerHTML = wins;
         computerGuess = computerOptions [Math.floor(Math.random() * computerOptions.length)];
@@ -37,11 +39,12 @@ window.onload = function() {
     } else {
         losses++;
         guessesLeft = 9;
-        playerGuessArray.length = 0;
+        guessedLetters = [];
+        var displayGuessedLetters = document.getElementById("lettersGuessed");
+        displayGuessedLetters.innerHTML = guessedLetters;
         var displayLosses = document.getElementById("losses");
         displayLosses.innerHTML = losses;
         computerGuess = computerOptions [Math.floor(Math.random() * computerOptions.length)];
-        console.log(losses);
         console.log(computerGuess);
         
         }
